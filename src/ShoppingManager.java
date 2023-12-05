7d4a79cc5839704377ddfceced0a0bb1b8919f66
MalithSrineth <malithsrineth@gmail.com>
2023-12-05 21:24:42 +0530

    Interface ShoppingManager Created

---
+++ src/ShoppingManager.java
@@ -0,0 +1,8 @@
+public interface ShoppingManager {
+    void addProduct(Product product);
+    void removeProduct(Product product);
+    void emptyCart();
+    void printProducts();
+    void fileWriter();
+    void fileReader();
+}
