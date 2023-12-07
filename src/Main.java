7facee8742dff59abc6fe26e9324ec54868da41d
MalithSrineth <malithsrineth@gmail.com>
2023-12-07 16:55:38 +0530

    Main Class Added

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
+        ArrayList<Product> products = WestminsterShoppingManager.getProducts();
+
+        String menu = "=======================================\n" +
+                      "Welcome to Westminster Shopping Manager\n" +
+                      "=======================================\n" +
+                      "1. Add a product\n" +
+                      "2. Remove a product\n" +
+                      "3. Print the list of products\n" +
+                      "4. Print the list of products in the cart\n" +
+                      "5. Empty the cart\n" +
+                      "6. Print the total price of the cart\n" +
+                      "7. Save the list of products to a file\n" +
+                      "8. Load the list of products from a file\n" +
+                      "9. Exit\n" +
+                      "=======================================\n" +
+                      "\nEnter your choice: ";
+        
+        int menuChoice = -1;
+
+        do {
+            System.out.print(menu);
+            menuChoice = WestminsterShoppingManager.getVerifiedIntInput();
+
+            switch (menuChoice) {
+                case 1:
+                    if (products.size() >= 1) {
+                        System.out.println("\nProduct limit reached\n");
+                    } else {
+                        int choice = -1;
+                        Product product = null;
+
+                        do {
+                            System.out.println("\n=====================================");
+                            System.out.println("Select the product type to add");
+                            System.out.println("1. Electronics");
+                            System.out.println("2. Clothes\n");
+                        
+                            choice = WestminsterShoppingManager.getVerifiedIntInput();
+                        
+                            switch (choice) {
+                                case 1:
+                                    product = new Electronics();
+                                    break;
+                                case 2:
+                                    product = new Clothing();
+                                    break;
+                                default:
+                                    System.out.println("Invalid choice. Please enter 1 for Electronics or 2 for Clothes.\n");
+                                    break;
+                            }
+                        } while (product == null);
+
+                        westminsterShoppingManager.addProduct(product, choice);
+                    }
+                    break;
+
+                case 2:
+                    // removeProduct(westminsterShoppingManager);
+                    break;
+                case 3:
+                    // printProducts(westminsterShoppingManager);
+                    break;
+                case 4:
+                    // printProductsInCart(westminsterShoppingManager);
+                    break;
+                case 5:
+                    // emptyCart(westminsterShoppingManager);
+                    break;
+                case 6:
+                    // printTotalPrice(westminsterShoppingManager);
+                    break;
+                case 7:
+                    // saveProductsToFile(westminsterShoppingManager);
+                    break;
+                case 8:
+                    // loadProductsFromFile(westminsterShoppingManager);
+                    break;
+                case 9:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 9.");
+                    break;
+            }
+        } while (menuChoice != 9);
+
+
+        System.out.println("=====================================");
+        System.out.println("Select the product type to add");
+        System.out.println("1. Electronics");
+        System.out.println("2. Clothes");
+            
+        int choice = WestminsterShoppingManager.getVerifiedIntInput();
+        Product product = null;
+
+        do {
+            System.out.println("=====================================");
+            System.out.println("Select the product type to add");
+            System.out.println("1. Electronics");
+            System.out.println("2. Clothes");
+        
+            choice = WestminsterShoppingManager.getVerifiedIntInput();
+        
+            switch (choice) {
+                case 1:
+                    product = new Electronics();
+                    break;
+                case 2:
+                    product = new Clothing();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter 1 for Electronics or 2 for Clothes.");
+                    break;
+            }
+        } while (product == null);
+
+        westminsterShoppingManager.addProduct(product, choice);
+    }
+}
--- src/WestminsterShoppingManager.java
+++ src/WestminsterShoppingManager.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class WestminsterShoppingManager implements ShoppingManager {
     static Scanner input = new Scanner(System.in);
     private static ArrayList<Product> products;
+    public static Object getProducts;
 
     public WestminsterShoppingManager() {
         WestminsterShoppingManager.products = new ArrayList<>(50);
@@ -88,7 +89,7 @@ public class WestminsterShoppingManager implements ShoppingManager {
         throw new UnsupportedOperationException("Unimplemented method 'fileReader'");
     }
 
-    private static int getVerifiedIntInput() {
+    public static int getVerifiedIntInput() {
         int inputInt = -1;
         do {
             try {
@@ -102,7 +103,7 @@ public class WestminsterShoppingManager implements ShoppingManager {
         return inputInt;
     }
 
-    private static double getVerifiedDoubleInput() {
+    public static double getVerifiedDoubleInput() {
         double inputDouble = -1.0;
         do {
             try {
@@ -116,125 +117,8 @@ public class WestminsterShoppingManager implements ShoppingManager {
         return inputDouble;
     }
 
-    public static void main(String[] args) {
-        WestminsterShoppingManager westminsterShoppingManager = new WestminsterShoppingManager();
-
-        String menu = "=======================================\n" +
-                      "Welcome to Westminster Shopping Manager\n" +
-                      "=======================================\n" +
-                      "1. Add a product\n" +
-                      "2. Remove a product\n" +
-                      "3. Print the list of products\n" +
-                      "4. Print the list of products in the cart\n" +
-                      "5. Empty the cart\n" +
-                      "6. Print the total price of the cart\n" +
-                      "7. Save the list of products to a file\n" +
-                      "8. Load the list of products from a file\n" +
-                      "9. Exit\n" +
-                      "=======================================\n" +
-                      "\nEnter your choice: ";
-        
-        int menuChoice = -1;
-
-        do {
-            System.out.print(menu);
-            menuChoice = getVerifiedIntInput();
-
-            switch (menuChoice) {
-                case 1:
-                    if (products.size() >= 1) {
-                        System.out.println("\nProduct limit reached\n");
-                    } else {
-                        int choice = -1;
-                        Product product = null;
-
-                        do {
-                            System.out.println("\n=====================================");
-                            System.out.println("Select the product type to add");
-                            System.out.println("1. Electronics");
-                            System.out.println("2. Clothes\n");
-                        
-                            choice = getVerifiedIntInput();
-                        
-                            switch (choice) {
-                                case 1:
-                                    product = new Electronics();
-                                    break;
-                                case 2:
-                                    product = new Clothing();
-                                    break;
-                                default:
-                                    System.out.println("Invalid choice. Please enter 1 for Electronics or 2 for Clothes.\n");
-                                    break;
-                            }
-                        } while (product == null);
-
-                        westminsterShoppingManager.addProduct(product, choice);
-                    }
-                    break;
-
-                case 2:
-                    // removeProduct(westminsterShoppingManager);
-                    break;
-                case 3:
-                    // printProducts(westminsterShoppingManager);
-                    break;
-                case 4:
-                    // printProductsInCart(westminsterShoppingManager);
-                    break;
-                case 5:
-                    // emptyCart(westminsterShoppingManager);
-                    break;
-                case 6:
-                    // printTotalPrice(westminsterShoppingManager);
-                    break;
-                case 7:
-                    // saveProductsToFile(westminsterShoppingManager);
-                    break;
-                case 8:
-                    // loadProductsFromFile(westminsterShoppingManager);
-                    break;
-                case 9:
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter a number between 1 and 9.");
-                    break;
-            }
-        } while (menuChoice != 9);
-
-
-        System.out.println("=====================================");
-        System.out.println("Select the product type to add");
-        System.out.println("1. Electronics");
-        System.out.println("2. Clothes");
-            
-        int choice = getVerifiedIntInput();
-        Product product = null;
-
-        do {
-            System.out.println("=====================================");
-            System.out.println("Select the product type to add");
-            System.out.println("1. Electronics");
-            System.out.println("2. Clothes");
-        
-            choice = getVerifiedIntInput();
-        
-            switch (choice) {
-                case 1:
-                    product = new Electronics();
-                    break;
-                case 2:
-                    product = new Clothing();
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please enter 1 for Electronics or 2 for Clothes.");
-                    break;
-            }
-        } while (product == null);
-
-        westminsterShoppingManager.addProduct(product, choice);
+    public static ArrayList<Product> getProducts() {
+        return products;
     }
-
     
 }
